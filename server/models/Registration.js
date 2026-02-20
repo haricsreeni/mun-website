18f1fb4843f452250e648875de14be7513ed216e
Madhav Pradeep <madhavpradeep2207@gmail.com>
2026-02-21 01:42:48 +0530

    added postgres

--- server/models/Registration.js
+++ server/models/Registration.js
@@ -1,62 +1,84 @@
-const mongoose = require('mongoose');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
 
-const registrationSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true, 'Full name is required'],
-    trim: true,
+const Registration = sequelize.define(
+  'Registration',
+  {
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Full name is required' },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Email is required' },
+        is: {
+          args: /^\S+@\S+\.\S+$/,
+          msg: 'Please enter a valid email',
+        },
+      },
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Phone number is required' },
+      },
+    },
+    institution: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Institution is required' },
+      },
+    },
+    delegationType: {
+      type: DataTypes.ENUM('individual', 'institution'),
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Delegation type is required' },
+      },
+    },
+    committee: {
+      type: DataTypes.ENUM('unsc', 'disec', 'unhrc', 'crisis'),
+      allowNull: false,
+      validate: {
+        notEmpty: { msg: 'Committee preference is required' },
+      },
+    },
+    delegatesCount: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+      validate: {
+        min: { args: [2], msg: 'Delegates count must be at least 2' },
+      },
+    },
+    experience: {
+      type: DataTypes.ENUM('yes', 'no'),
+      allowNull: true,
+      defaultValue: null,
+    },
+    accommodation: {
+      type: DataTypes.ENUM('yes', 'no'),
+      allowNull: true,
+      defaultValue: null,
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      defaultValue: '',
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    trim: true,
-    lowercase: true,
-    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email'],
-  },
-  phone: {
-    type: String,
-    required: [true, 'Phone number is required'],
-    trim: true,
-  },
-  institution: {
-    type: String,
-    required: [true, 'Institution is required'],
-    trim: true,
-  },
-  delegationType: {
-    type: String,
-    enum: ['individual', 'institution'],
-    required: [true, 'Delegation type is required'],
-  },
-  committee: {
-    type: String,
-    enum: ['unsc', 'disec', 'unhrc', 'crisis'],
-    required: [true, 'Committee preference is required'],
-  },
-  delegatesCount: {
-    type: Number,
-    min: 2,
-    default: null,
-  },
-  experience: {
-    type: String,
-    enum: ['yes', 'no'],
-    default: null,
-  },
-  accommodation: {
-    type: String,
-    enum: ['yes', 'no'],
-    default: null,
-  },
-  message: {
-    type: String,
-    trim: true,
-    default: '',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    tableName: 'registrations',
+    timestamps: true,
+    updatedAt: false,
+  }
+);
 
-module.exports = mongoose.model('Registration', registrationSchema);
+module.exports = Registration;
